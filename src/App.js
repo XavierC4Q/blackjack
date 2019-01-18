efce1f73f9e8972d15237cd3a5a2a5b43211679c
Xavier <xaviermunroe@ac.c4q.nyc>
2019-01-18 15:10:13 -0500

    now count cards values, handles aces, gives initial hand to user and dealer. Next will work on CSS to show both hands, will make a better way to handle the aces (cannot see other cards in hand before picking the value of the Ace) and getting betting down

--- src/App.js
+++ src/App.js
@@ -4,49 +4,108 @@ import logo from './logo.svg';
 import './App.css';
 import { Card } from './Card'
 
+const cardValues = {
+  "1": 1,
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "10": 10,
+  "KING": 10,
+  "QUEEN": 10,
+  "JACK": 10
+}
+
+
 class App extends Component {
   state = {
-    deckID: null,
+    deck_id: null,
     remaining: 0,
-    cards: []
+    cards: [],
+    handValue: 0,
+    dealerCards: [],
+    dealerHandValue: 0
   }
 
   componentDidMount(){
-    const deckURL = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
-    axios.get(deckURL)
-    .then(res => {
-      this.setState({
-        deckID: res.data.deck_id,
-        remaining: res.data.remaining
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    this.fillDeck()
   }
 
-  handleCardDraw = () => {
-    const { deckID } = this.state
-    const url = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=3`;
+  fillDeck = async () => {
+    const deckURL = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`;
+    try {
+      const fillDeck = await axios.get(deckURL)
+      const { data: { deck_id, remaining } } = fillDeck
 
-    axios.get(url)
-    .then(res => {
-      console.log('BEFORE SET STATE', this.state.cards)
-      console.log('RES CARD', res.data.cards)
-      this.setState({
-        cards: [...this.state.cards, ...res.data.cards],
-        remaining: res.data.remaining
-      })
-      console.log('AFTER SET STATE', this.state.cards)
-    })
+      this.setState(prevState => ({ deck_id , remaining }))
+
+      this.initialDraw(this.state.deck_id)
+    }
+    catch(err){
+      console.log('Error filling Deck')
+    }
+  }
+
+  initialDraw = async (deck_id) => {
+    const drawCardUrl = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=4`
+    try {
+      const getInitialHands = await axios.get(drawCardUrl)
+      const { data: { cards, remaining } } = getInitialHands
+
+      let cardsForDealer = cards.slice(0, 2)
+      let cardsForUser = cards.slice(2)
+
+      this.setState(prevState => ({ 
+        dealerCards: [...cardsForDealer], 
+        cards: [...cardsForUser],
+        handValue: this.getCardValues(...cardsForUser),
+        dealerHandValue: this.getCardValues(cardsForDealer[1]),
+        remaining 
+      }))
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  getCardValues = (...cards) => {
+    let getCardValues = Object.values(cards)
+    let findValue = getCardValues.reduce((total, c) => {
+      if(c.value === 'ACE'){
+        return total += this.handleAce()
+      }
+      return total += cardValues[c.value]
+    }, 0)
+
+    return findValue
+  }
+
+  handleAce = () => {
+    let pickAceValue = prompt(
+      `You have drawn an ace! Do you want it to count for 1 or for 10?`
+      )
+    if(!isNaN(pickAceValue)){
+      if(Number(pickAceValue) !== 1 && Number(pickAceValue) !== 10){
+        this.handleAce()
+      }
+      return Number(pickAceValue)
+    }
+    this.handleAce()
+  }
+
+  hitMe = async () => {
+
   }
 
   render() {
-    console.log(this.state)
     return (
       <div>
         <h1>Blackjack</h1>
-        <button onClick={this.handleCardDraw}>Draw a Card</button>
+        <button onClick={this.handleCardDraw}>Hit Me</button>
         <div className='card-container'>
           {this.state.cards.map(card => (<Card card={card} />))}
         </div>
